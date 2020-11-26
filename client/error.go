ae80eb2f902c63b21ae13637a3157f0ad7c8c4ea
Tim Schindler <xh3b4sd@gmail.com>
2020-11-26 18:37:52 +0100

    prevent creating duplicates

--- client/error.go
+++ client/error.go
@@ -7,11 +7,20 @@ import (
 	"github.com/xh3b4sd/tracer"
 )
 
-// executionFailedError is an error type for unexpected situations where further
-// code execution cannot continue. This error should never be matched against.
-// Therefore there is no error matcher implemented.
+var alreadyExistsError = &tracer.Error{
+	Kind: "alreadyExistsError",
+}
+
+func IsAlreadyExistsError(err error) bool {
+	return errors.Is(err, alreadyExistsError)
+}
+
 var executionFailedError = &tracer.Error{
 	Kind: "executionFailedError",
+}
+
+func IsExecutionFailedError(err error) bool {
+	return errors.Is(err, executionFailedError)
 }
 
 var invalidConfigError = &tracer.Error{
